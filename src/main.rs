8beaac249260dc3b362fa74ffc012321e384d8c1
Dennis Schulze <dennis@axelschulze.net>
2022-08-14 14:03:14 +0200

    added flag to level the artifacts

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,49 @@ use artifact::*;
 
 fn usage() {
   let prog_anme = &std::env::args().take(1).collect::<Vec<String>>()[0];
-  println!("{} <count>", prog_anme)
+  println!("{} [<options>] <count>", prog_anme);
+  println!("  Options:" );
+  println!("   -l           level the artifact to +20");
+  println!("   -h, --help   print this help");
 }
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
   let input = &args[1..];
+  let last_value = input.last();
+  let should_level = args.iter().find( |&e| e == "-l" );
 
-  match input[0].parse::<u32>() {
-    Ok(val) => {
+  if let Some(_) = args.iter().find( |&e| e == "-h" || e == "--help" ) {
+    usage();
+    return
+  }
+
+  if last_value == None {
+    eprintln!("ERROR: not enough arguments provided");
+    usage();
+    return
+  }
+
+  match (last_value.unwrap().parse::<u32>(), should_level) {
+    (Ok(val), Some(_)) => {
+      for i in 0..val {
+        let mut a = generate_arifact(5);
+        println!("new:\n{}", a);
+        a.add_levels(20);
+        println!("leveled:\n{}", a);
+        if i < val - 1 {
+          println!("----------------------");
+        }
+      }
+    },
+    (Ok(val), None) => {
       for _ in 0..val {
         println!("{}", generate_arifact(5));
       }
     },
-    Err(error) => {
+    (Err(error), _) => {
       eprintln!("ERROR: {}", error);
       usage()
     },
   }
-
-  // for _ in 0..20 {
-  //   let mut a = generate_arifact(5);
-  //   println!("{}", a);
-  //   a.add_levels(20);
-  //   println!("{}", a);
-  //   println!("----------------------------");
-  // }
 }
