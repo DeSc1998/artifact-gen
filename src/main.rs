be70d533a3253a41a3fef2d5c0efcd2e442fdfd0
Dennis Schulze <dennis@axelschulze.net>
2022-08-14 13:20:24 +0200

    generate artifacts based on program argument

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,35 @@ mod artifact;
 mod stat;
 mod statgrowth;
 
+#[warn(unused_imports)]
 use artifact::*;
 
+fn usage() {
+  let prog_anme = &std::env::args().take(1).collect::<Vec<String>>()[0];
+  println!("{} <count>", prog_anme)
+}
+
 fn main() {
-  for _ in 0..20 {
-    let mut a = generate_arifact(5);
-    println!("{}", a);
-    a.add_levels(20);
-    println!("{}", a);
-    println!("----------------------------");
+  let args: Vec<String> = std::env::args().collect();
+  let input = &args[1..];
+
+  match input[0].parse::<u32>() {
+    Ok(val) => {
+      for _ in 0..val {
+        println!("{}", generate_arifact(5));
+      }
+    },
+    Err(error) => {
+      eprintln!("ERROR: {}", error);
+      usage()
+    },
   }
+
+  // for _ in 0..20 {
+  //   let mut a = generate_arifact(5);
+  //   println!("{}", a);
+  //   a.add_levels(20);
+  //   println!("{}", a);
+  //   println!("----------------------------");
+  // }
 }
